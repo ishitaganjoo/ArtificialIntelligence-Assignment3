aa624990b25b9ac3610ef8c5b14c9132c027b3ec
syampara <syampara@iu.edu>
2016-11-04 04:36:37 -0400

    Complex algorithm using virtebi and transition probability for complex algorithm

--- part1/pos_solver.py
+++ part1/pos_solver.py
@@ -30,6 +30,8 @@ class Solver:
         self.viterbiStateDict = {}
         self.mostLikelyStateSeqDict = {}
         self.mostLikelyPOSList = []
+        self.complexTransitionProb={}
+        self.transitDict={}
     # Calculate the log of the posterior probability of a given sentence
     #  with a given part-of-speech labeling
     def posterior(self, sentence, label):
@@ -71,25 +73,34 @@ class Solver:
                 else:
                     self.emissionProbDict[(item[0][i],item[1][i])] = self.emissionProbDict[(item[0][i],item[1][i])] + 1
 
+            for i in range(2,len(item[1])):
+                if i<len(item[1])-2:
+                    if (item[1][i+2],item[1][i]+'/'+item[1][i+1]) not in self.complexTransitionProb:
+                        self.complexTransitionProb[(item[1][i+2],item[1][i]+'/'+item[1][i+1])] = 1
+                    else:
+                        self.complexTransitionProb[(item[1][i+2],item[1][i]+'/'+item[1][i+1])] = self.complexTransitionProb[(item[1][i+2],item[1][i]+'/'+item[1][i+1])] + 1
+
+
         sumInitialProb = sum(self.initialProbDict.values())
         self.initialProbDict.update({n: float( self.initialProbDict[n])/ float(sumInitialProb)for n in self.initialProbDict.keys()})
-        
-        listKeysTranstn = self.transitionProbDict.keys()
-        # for key in listKeysTranstn:
-        #     self.transitionProbDict[key] = float(self.transitionProbDict[key])/float(self.countPosDict[key[0]])
 
-        for key_1,value_1 in self.transitionProbDict.items():
-            den_sum = 0
-            for key_2,value_2 in self.transitionProbDict.items():
-                if key_1[0] == key_2[0]:
-                    den_sum = float(den_sum) + float(value_2)
-            self.transitionProbDict[key_1] = float(value_1) / float(den_sum)
-
-
+        self.updateTransitions(self.transitionProbDict)
+        self.updateTransitions(self.complexTransitionProb)
         listKeys = self.emissionProbDict.keys()
         for key in listKeys:
             self.emissionProbDict[key] = float(self.emissionProbDict[key])/float(self.countPosDict[key[1]])
         pass
+
+    #function to update the transition probabilities of one-oreder model and two-order model
+
+    def updateTransitions(self,dict):
+        for key_1,value_1 in dict.items():
+            den_sum = 0
+            for key_2,value_2 in dict.items():
+                if key_1[0] == key_2[0]:
+                    den_sum = float(den_sum) + float(value_2)
+            dict[key_1] = float(value_1) / float(den_sum)
+
 
     # Functions for each algorithm.
     #
@@ -97,35 +108,41 @@ class Solver:
         mostPosDict=self.mostPosDict
         for i in range(0,len(sentence)):
             maxprob=0;
-            for j in range(0,len(self.countPosDict.keys())):
-                dictKey=(sentence[i],self.countPosDict.keys()[j]) #changed to tuple
+            if sentence[i] not in self.countWordsDict.keys():
+                self.countWordsDict[sentence[i]]=0.000001
+            for j in self.countPosDict:
+                dictKey=(sentence[i],j) #changed to tuple
                 if dictKey in self.emissionProbDict.keys():
-                    currentprob = float(self.emissionProbDict[dictKey]) * float(self.countPosDict[self.countPosDict.keys()[j]])/float(self.countWordsDict[sentence[i]])
+                    currentprob = float(self.emissionProbDict[dictKey]) * float(self.countPosDict[j])/float(self.countWordsDict[sentence[i]])
                 else:
                     currentprob = 0.000001
                 if maxprob < currentprob:
                     maxprob = currentprob
-                    mostPosDict[sentence[i]] = self.countPosDict.keys()[j]+'@'+str(maxprob)
+                    mostPosDict[sentence[i]] = j+'@'+str(maxprob)
 
         return [[ [mostPosDict[sentence[i]].split('@')[0] for i in range(len(sentence))]], [ ['%.2f'%(float(mostPosDict[sentence[i]].split('@')[1])) for i in range(len(sentence))], ]]
 
     def returnMax(self,j,listPOS,emissionProb):
         maxProb = 0
         path = ''
+
         for i in listPOS:
             if (i,j) not in self.transitionProbDict:
                 self.transitionProbDict[(i,j)] = 0.000001
+            # print self.viterbiStateDict
             prob = self.viterbiStateDict[i] * self.transitionProbDict[(i,j)]
             if prob > maxProb:
                 maxProb = prob
                 path = i
         self.mostLikelyStateSeqDict[j] = maxProb*emissionProb
+
         return maxProb
     
     def hmm(self, sentence):
         #print("transition prob dict", self.transitionProbDict)
         self.mostLikelyPOSList = [] #empty it for each sentence
         listPOS = list(set(self.initialProbDict.keys()))
+        
         for i in range(0,len(sentence)):
             for j in range(0,len(listPOS)):
                 if (sentence[i],listPOS[j]) not in self.emissionProbDict:
@@ -143,14 +160,56 @@ class Solver:
             key, _ = max(self.mostLikelyStateSeqDict.iteritems(), key=lambda x:x[1])
             self.mostLikelyPOSList.append(key)
             self.mostLikelyStateSeqDict = {}
-                 
-        print("sentence is", sentence)
-
-        print("most likely pos is",self.mostLikelyPOSList)
         return [[[self.mostLikelyPOSList[n] for n in range(len(sentence))]], []]
 
     def complex(self, sentence):
-        return [ [ [ "noun" ] * len(sentence)], [[0] * len(sentence),] ]
+
+        listPOS = list(set(self.initialProbDict.keys()))
+        wordList = list(set(self.countWordsDict))
+        for i in listPOS:
+            for j in listPOS:
+                if (i,j) not in self.transitionProbDict:
+                    self.transitionProbDict[(i,j)] = 0
+                self.transitDict[(i,j)] = (i,self.transitionProbDict[(i,j)])
+        # print transitDict[('adv','adv')]
+        for word in sentence:
+            mostLikelyPOSDict = {}
+            for next_pos in listPOS:
+                argmax = None
+                valmax = 0
+                for current_pos in listPOS:
+                    for prev_pos in listPOS:
+                        if (current_pos,prev_pos) not in self.transitDict:
+                            self.transitDict[(current_pos, prev_pos)] = (None,0)
+                        (v_path,v_prob)=self.transitDict[(current_pos,prev_pos)]
+                        if (word,current_pos) not in self.emissionProbDict:
+                            self.emissionProbDict[(word,current_pos)] = 0.000001
+                        if (next_pos,current_pos+'/'+prev_pos) not in self.complexTransitionProb:
+                            self.complexTransitionProb[(next_pos,current_pos+'/'+prev_pos)] = 0.000001
+                        emissionprob = self.emissionProbDict[(word,current_pos)] * self.complexTransitionProb[(next_pos,current_pos+'/'+prev_pos)]
+                        v_prob *= emissionprob
+                        if v_prob > valmax:
+                            argmax = v_path +','+ next_pos
+                            valmax = v_prob
+                        mostLikelyPOSDict[(next_pos,current_pos)] = (argmax, valmax)
+            self.transitDict=mostLikelyPOSDict
+
+        total = 0
+        argmax = None
+        valmax = 0
+        for state1 in listPOS:
+            for state2 in listPOS:
+                try:
+                    (v_path, v_prob) = self.transitDict[(state1,state2)]
+                except KeyError:
+                    (v_path, v_prob) = self.transitDict[(state1,state2)] = (None, 0)
+
+                if v_prob > valmax:
+                    argmax = v_path
+                    valmax = v_prob
+
+        argList=argmax.split(',')
+        return [ [ [ argList[:-1][n] for n in range(len(sentence))] ], [[0] * len(sentence),] ]
 
 
     # This solve() method is called by label.py, so you should keep the interface the
